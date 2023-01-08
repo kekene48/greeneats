646825ab7b762e54c9dd0500d10c4a94cbfdfa4d
emie <87718058+emielol@users.noreply.github.com>
2023-01-08 17:51:18 -0500

    Adding buttons

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -4,23 +4,30 @@ import './login.css'
 
 const Login = () => {
   return (
+    <body>
     <header className='login__container'>
+      <div class="wrapper">
+        <a href="#" class="home"><span>GreenEats</span></a>
+      </div>
         <div className="login">
-          <div className="login__top">
-            <img src={logo} alt="logo" className='login__logo'/>
-            <h1 className="login__h1">Login</h1>
-            <img src={logo} alt="logo" className='login__logo'/>
-          </div>
-          <form action="/" method="post" className='login__form'>
-            <input type="text" placeholder='Username' name='username' className='login__input-username login__input'/>
-            <input type="text" placeholder='Password' name='password' className='login__input-password login__input'/>
-          </form>
-          <a href="https://google.com" className='btn login__btn' target='__blank'>Login</a>
-          <div className="signup">New?
-            <a href="https://google.com" target='__blank' className='signup__a'>  SignUp</a>
-          </div>
+          <form action="/login" method="post" className="login__form">
+            <div className="login__top">
+              <img src={logo} alt="logo" className='login__logo'/>
+              <h1 className="login__h1">Log in!</h1>
+              <img src={logo} alt="logo" className='login__logo'/>
+            </div>
+            <input type="text" placeholder="Username" name="username" className="login__input-username login__input form-control fader"/>
+            <input type="text" placeholder="Password" name='password' className='login__input-password login__input form-control fader'/>
+            <button type="submit"  class="btn btn-success login__btn fader">Log in</button>
+            <div className="signup fader">
+              New?<a href="https://google.com" target='__blank' className='signup__a '> Create your account</a>
+            </div>
+    
+          </form> 
         </div>
     </header>
+    </body>
+
   )
 }
 
